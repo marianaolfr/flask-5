02d80d2f233e8838c8f445a964bc4794e77d099b
Mariana Oliveira <91221958+marianaolfr@users.noreply.github.com>
2024-03-21 20:59:25 -0300

    Update change_repo.sh

--- change_repo.sh
+++ change_repo.sh
@@ -4,5 +4,5 @@ git remote remove origin
 git remote add origin $1
 git fetch --all
 git reset --hard origin/main
-git branch --set-upstream-to=origin/main main
+git branch --set-upstream-to=origin/master master
 git pull
